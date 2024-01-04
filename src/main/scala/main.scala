7486d42a7a4407d36c63c5cc0128d2a215bf2c14
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2024-01-04 16:03:45 +0100

    Implement final exception handling example

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,31 @@
 @main
 def main(): Unit = {
   println("Hello world!")
-  var p1 = new Pojazd("Ford", "Focus")
-  var p2 = new Pojazd("Ford", "Focus", 2010)
-  var p3 = new Pojazd("Ford", "Focus", 2010, "DW 12345")
+  val p1 = new Pojazd("Ford", "Focus")
+  val p2 = new Pojazd("Ford", "Focus", 2010)
+  val p3 = new Pojazd("Ford", "Focus", 2010, "DW 12345")
 
-  var t31 = new Time3(1, 5)
+  val t31 = new Time3(1, 5)
   println(t31.getHour)
   println(t31.getMinute)
+
+  try {
+    metoda1()
+  } catch {
+    case e: Exception =>
+      println("Wyjatek: " + e.getMessage)
+      e.printStackTrace()
+  }
+  /*
+  at main$package$.metoda1(main.scala:78)
+  at main$package$.main(main.scala:13)
+  main$package$ to nazwa pakietu, w którym znajduje się metoda main
+  main to nazwa pliku, w którym znajduje się metoda main
+  .metoda1 to nazwa metody w której wywołano wyjątek
+  13 to numer linii, w której wywołano metodę metoda1
+
+  */
+
 }
 
 class Time1(private var hour: Int):
@@ -65,3 +83,15 @@ end Time3
 //Parametry rokProdukcji i rejestracja są opcjonalne bo mają wartości domyślne, nie trzeba ich podawać przy tworzeniu obiektu.
 class Pojazd(val producent: String, val model: String, val rokProdukcji: Int = -1, var rejestracja: String = "")
 
+@throws[Exception]
+def metoda1(): Unit = {
+  metoda2()
+}
+@throws[Exception]
+def metoda2(): Unit = {
+  metoda3()
+}
+@throws[Exception]
+def metoda3(): Unit = {
+  throw new Exception("Wyjatek zgloszony w metoda3")
+}
